99526ba13c6e3a319ca2c9a236a94a4780dffc02
João Rafael Menezes <menezes.jrafael@gmail.com>
2024-01-23 22:45:43 -0300

    Criando category

--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,16 @@ import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 import { isAuthenticaded } from './middlewares/isAuthenticated';
+import { CreateCategoryController } from './controllers/category/CreateCategoryController';
+import { ListCategoryController } from './controllers/category/ListCategoryController';
 
 const router = Router();
 
 router.post('/users', new CreateUserController().handle);
 router.post('/session', new AuthUserController().handle);
-
 router.get('/me', isAuthenticaded, new DetailUserController().handle);
+//Rotas de categoria
+router.post('/category', isAuthenticaded, new CreateCategoryController().handle);
+router.get('/categoryList', isAuthenticaded, new ListCategoryController().handle);
 
 export { router };
